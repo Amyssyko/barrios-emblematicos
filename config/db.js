9a887bd300263339b8399accc7c2262e83ec354f
Christian Pilla <cristian-0005@live.com>
2023-01-21 13:12:00 -0500

    octavo cambio

--- config/db.js
+++ config/db.js
@@ -1,12 +1,12 @@
 import { createPool } from "mysql2/promise"
-//const dotenv = require("dotenv")
+const dotenv = require("dotenv")
 
 const pool = createPool({
-  host: "sql10.freemysqlhosting.net",
-  user: "sql10592184",
-  password: "RdXSV5yZmc",
-  port: "3306",
-  database: "sql10592184",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
